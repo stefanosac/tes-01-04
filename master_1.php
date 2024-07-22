6e62ca68e9b736ebb2e25a0f9066b287e09e9168
stefanosac <stefano.sac@gmail.com>
2024-07-22 14:22:19 +0200

    aggiunto file master_1.php

---
+++ master_1.php
@@ -0,0 +1,13 @@
+<?php
+require_once "autoload.php";
+
+
+use App01\Rootto; // as Rootto;
+
+echo 'io sono entry.php su porta ' . $_SERVER['SERVER_PORT'];
+
+$rot = new Rootto();
+echo $rot->getId();
+/* ho aggiunto un commento */
+/* altro commento */
+?>
